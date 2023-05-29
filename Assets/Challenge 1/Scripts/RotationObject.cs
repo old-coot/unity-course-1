78b9374770f4702f1ac72592f3a8eb4ea87ef188
old-coot <knightsofthewharf@gmail.com>
2023-05-29 21:55:43 +0300

    unit 1 completed

--- Assets/Challenge 1/Scripts/RotationObject.cs
+++ Assets/Challenge 1/Scripts/RotationObject.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class RotationObject : MonoBehaviour
 {
+    public float rotationSpeed;
+    float bigValue = 45.6f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.fwd * rotationSpeed);
     }
 }
